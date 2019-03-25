37c33129c1b1b2df6e217438bffdc5f8eca4ff81
Pavel <Pawelv9@gmail.com>
2019-03-25 23:05:27 +0300

    Logo component

--- face-recognition-game/src/components/Logo/Logo.js
+++ face-recognition-game/src/components/Logo/Logo.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import Tilt from 'react-tilt';
+import './Logo.css';
+import ufo from './ufo.png'
 
 export default function Logo() {
   return (
     <div className='ma4 mt0'>
-      
+          <Tilt className="Tilt br2 shadow-2" options={{ max: 55 }} style={{ height: 200, width: 200 }} >
+              <div className="Tilt-inner"> <img style={{paddingTop: '45px'}} src={ufo} alt="logo"/></div>
+          </Tilt>
     </div>
   )
 }
