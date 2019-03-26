f5fbef77e6a0e805969f140ace0c1d380559ff38
Pavel <Pawelv9@gmail.com>
2019-03-26 23:30:24 +0300

    adding a Rank and background

--- face-recognition-game/src/App.js
+++ face-recognition-game/src/App.js
@@ -1,17 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import Particles from 'react-particles-js';
 
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 80,
+      density: {
+        enable: true,
+        value_area: 800,
+        // color: "#3CA9D1",
+        // blur: 5
+      }
+    }
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Particles className='particles'
+          params={particlesOptions} />
         <Navigation />
         <Logo />
+        <Rank />
         <ImageLinkForm />
         {/* <FaceRecognition /> */}
       </div>
--- face-recognition-game/src/components/Logo/Logo.js
+++ face-recognition-game/src/components/Logo/Logo.js
@@ -6,8 +6,8 @@ import ufo from './ufo.png'
 export default function Logo() {
   return (
     <div className='ma4 mt0'>
-          <Tilt className="Tilt br2 shadow-2" options={{ max: 55 }} style={{ height: 200, width: 200 }} >
-              <div className="Tilt-inner"> <img style={{paddingTop: '45px'}} src={ufo} alt="logo"/></div>
+          <Tilt className="Tilt br2 shadow-2" options={{ max: 55 }} style={{ height: 150, width: 150 }} >
+              <div className="Tilt-inner"> <img style={{paddingTop: '25px'}} src={ufo} alt="logo"/></div>
           </Tilt>
     </div>
   )
